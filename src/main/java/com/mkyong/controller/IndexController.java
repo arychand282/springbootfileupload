76e15242169b5bd2c1854b08d188aa6308b032b1
Bagus Ari Chandra <b.chandra@teravintech.com>
2017-11-21 14:19:18 +0700

    addingGetList

--- src/main/java/com/mkyong/controller/IndexController.java
+++ src/main/java/com/mkyong/controller/IndexController.java
@@ -1,13 +1,27 @@
 package com.mkyong.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+
+import java.io.IOException;
+import java.util.stream.Collectors;
 
 @Controller
 public class IndexController {
 
+    @Autowired
+    private RestUploadController restUploadController;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) throws IOException {
+
+        model.addAttribute("files", restUploadController.getListUploadedFiles().map(
+                path -> MvcUriComponentsBuilder.fromMethodName(RestUploadController.class,
+                        "serveFile", path.getFileName().toString()).build().toString()).collect(Collectors.toList()));
+
         return "upload";
     }
 }
--- src/main/java/com/mkyong/controller/RestUploadController.java
+++ src/main/java/com/mkyong/controller/RestUploadController.java
@@ -1,25 +1,31 @@
 package com.mkyong.controller;
 
+import com.mkyong.exception.StorageException;
+import com.mkyong.exception.StorageFileNotFoundException;
 import com.mkyong.model.UploadModel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RestController
 public class RestUploadController {
@@ -27,7 +33,9 @@ public class RestUploadController {
     private final Logger logger = LoggerFactory.getLogger(RestUploadController.class);
 
     // save the uploaded file to this folder
-    private static String UPLOADED_FOLDER = "/home/arychand/idea-IC-172.4343.14/Data/a_PRACTICE_SPRING_BOOT/sbfileupload/uploaded_folder/";
+    private static String UPLOADED_FOLDER = "uploaded_folder/";
+
+    private Path folderPath = Paths.get(UPLOADED_FOLDER);
 
     // save file
     private void saveUploadedFiles(List<MultipartFile> files) throws IOException {
@@ -42,6 +50,44 @@ public class RestUploadController {
             Files.write(path, bytes);
         }
 
+    }
+
+    public Stream<Path> getListUploadedFiles() {
+        logger.debug("Get List Uploaded Files");
+
+        try {
+            return Files.walk(folderPath, 1)
+                    .filter(path -> !path.equals(folderPath))
+                    .map(path -> folderPath.relativize(path));
+        } catch (IOException e) {
+            throw new StorageException("Failed to read stored files", e);
+        }
+    }
+
+    private Resource loadAsResource(String filename) {
+        try {
+            Path file = folderPath.resolve(filename);
+            Resource resource = new UrlResource(file.toUri());
+
+            if (resource.exists() || resource.isReadable()) {
+                return resource;
+            }
+            else {
+                throw new StorageFileNotFoundException("Could not read file: " + filename);
+            }
+
+        } catch (MalformedURLException e) {
+            throw new StorageFileNotFoundException("Could not read file: " + filename, e);
+        }
+    }
+
+    @GetMapping("/files/{filename:.+}")
+    @ResponseBody
+    public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
+        Resource file = loadAsResource(filename);
+        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
+                "attachment; filename=\"" + filename + "\"")
+                .body(file);
     }
 
     // single file upload
